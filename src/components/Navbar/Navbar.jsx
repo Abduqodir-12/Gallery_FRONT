edb65f2721a621072fcdae4eb5be52de230ca1a3
Abduqodir-12 <abduqodirabdujabbarov60@gmail.com>
2024-10-29 17:14:33 +0500

    header bitdi

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { useInfoContext } from "../../context/Context";
 import logout from '../../img/log out.png';
+import logo from '../../img/logoImg.jpg';
 
 const Navbar = () => {
   const { exit, searchTerm, setSearchTerm } = useInfoContext();
@@ -9,7 +10,7 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand navLogo" href="/">Abduqodir Gallery</a>
+          <a className="navbar-brand navLogo" href="/">Abduqodir <img className="logoIcon" src={logo} alt="logo" /></a>
           <form className="d-flex w-100" role="search">
             <input type="text" className="form-control me-2 w-100" placeholder='Search' name='name' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
             <button className="nav-link btn px-2 mx-2 bg-danger" onClick={exit} aria-current="page"><img className="btnIcons" src={logout} alt="logOut" /></button>
